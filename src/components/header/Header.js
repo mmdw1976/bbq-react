1c34d060ac5f25ac6066453115e6d183a29e2e30
Mario <mmdw1976@gmail.com>
2022-03-08 17:56:24 +0100

    Navbar links placed within ul tag and adjusted nav styling and tweaking

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,20 +5,22 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
-    return ( 
-        <header>
-            <div className="header-bg"></div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/recepten">Recepten</Link>
-                <Link to="/about">About</Link>
-            </nav>
-            <div className="header-content shadow-box">
-                <h1>Mario's BBQ Place</h1>
-                <p>Welcome bij Mario's BBQ place</p>
-            </div>
-        </header>
-     );
+    return (
+      <header>
+        <div className='header-bg'></div>
+        <nav>
+          <ul>
+            <Link to='/'>Home</Link>
+            <Link to='/recepten'>Recepten</Link>
+            <Link to='/about'>About</Link>
+          </ul>
+        </nav>
+        <div className='header-content shadow-box'>
+          <h1>Mario's BBQ Place</h1>
+          <p>Welcome bij Mario's BBQ place</p>
+        </div>
+      </header>
+    );
 }
  
 export default Header;
